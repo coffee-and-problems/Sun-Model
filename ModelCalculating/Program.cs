9a07c9e8e54bb77cd6350f36b3bca7f85ef4758d
Maria <marychobanu@gmail.com>
2020-07-06 20:28:00 +0300

    Добавлен просмотр и сохранение хэдеров

---
+++ ModelCalculating/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace ModelCalculating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"C:\Users\julie\source\repos\Sun Model\Data\2D_logT\BIFROST_en096014_gol_lgtg_281.fits";
+            var fh = new FitsHandler();
+            //fh.PrintHeader(filePath);
+            fh.SaveHeaderAsync(filePath, @"C:\Users\julie\source\repos\Sun Model\Headers");
+        }
+    }
+}
